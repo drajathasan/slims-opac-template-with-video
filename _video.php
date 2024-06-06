928af0a9b55d0dc23a2fbed193deb47bb1d649a1
Drajat Hasan <38057222+drajathasan@users.noreply.github.com>
2024-06-07 05:15:23 +0700

    Create _video.php

---
+++ _video.php
@@ -0,0 +1,48 @@
+<style>
+    @media only screen and (max-width: 600px) {
+        video {
+            width: 800px !important;
+            height: 450px !important;
+        }
+    }
+
+    .card-body {
+        padding: 1.55rem;
+    }
+
+    #slims-home {
+        background: white;
+        position: relative;
+        top: -76px;
+    }
+
+    #slims-home > .container > h4 {
+        padding-top: 49px;
+    }
+
+    #outercontainer {
+        position: relative;
+        background: white;
+        /* top: -40px; */
+        padding-top: 50px
+    }
+
+    .search {
+        position: absolute;
+        z-index: 5;
+        width: 100%;
+    <?php if (isset($_GET['search']) || isset($_GET['p'])): ?>
+        margin-top: -11px;
+        top: 127px;
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+    <?php else: ?>
+        top: 430px;
+    <?php endif; ?>
+    }
+}
+</style>
+<video autoplay muted loop id="myVideo" style="width: 100%;position: absolute;">
+    <source src="<?= SWB ?>template/default/assets/videos/rain.mp4" type="video/mp4">
+</video>
